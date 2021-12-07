6bf499d1859105fc238f4fae07087d792c30f410
rk2-182 <ricardoplus@live.cl>
2021-12-06 22:42:52 -0300

    se crea fichero independiente para conexion a bd

--- usuario.py
+++ usuario.py
@@ -1,31 +1,31 @@
 #Clase modelo la cual interactua con la BD (insertando,actualizando,etc)
 import datetime
-import mysql.connector
 import hashlib
 
-database = mysql.connector.connect(
-    host = "localhost",
-    user="root",
-    password="root",
-    database="escuela",
-    #port="3306"
+from conexion import conectar
 
-)
-#print(database)
-cursor = database.cursor(buffered=True)
+conexion = conectar()
+
+bd = conexion[0]
+cursor = conexion[1]
+
+
 
 class Usuario:
+
+
 
     def __init__(self,nombre,apellidos,email,password):
         self.nombre = nombre
         self.apellidos = apellidos
         self.email = email
         self.password = password
-
+        
+       
     
     def registrarUsuario(self):
         fecha = datetime.datetime.now()
-        
+
         cifrar = hashlib.sha256()
         cifrar.update(self.password.encode('utf-8')) #codificar a bytes
 
@@ -38,7 +38,7 @@ class Usuario:
             #Ejecutar codigo sql y remplazar los datos
             cursor.execute(sql,usuario)
             #confirmar cambios
-            database.commit()
+            bd.commit()
         except:
             print('error, email ya registrado.')
         return [cursor.rowcount]
